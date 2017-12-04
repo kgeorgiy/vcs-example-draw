501ffd350abf38bab463a191b984c43b0879d6fd
Georgiy Korneev <kgeorgiy@keorgiy.info>
2017-12-04 12:37:33 +0300

    1) h and w variables fixed 2) Second line added 3) .ignore added

--- src/info/kgeorgiy/draw/Draw.java
+++ src/info/kgeorgiy/draw/Draw.java
@@ -12,13 +12,15 @@ import java.util.List;
 public class Draw {
 
     public static void main(String[] args) throws IOException {
-        int h = 1024;
-        int w = 512;
-        BufferedImage image = ImageUtil.createImage(h, w);
+        int w = 1024;
+        int h = 512;
+        BufferedImage image = ImageUtil.createImage(w, h);
         Graphics g = image.getGraphics();
 
-        Line line = new Line(0, 0, h, w, Color.WHITE);
-        line.draw(g);
+        Line line1 = new Line(0, 0, w, h, Color.WHITE);
+        Line line2 = new Line(0, h, w, 0, Color.WHITE);
+        line1.draw(g);
+        line2.draw(g);
 
         ImageUtil.writeImage("image.jpg", image);
         ImageUtil.showImage(image);
